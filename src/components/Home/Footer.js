88509ea11030eb1d3eac52683fda4a72168df48e
Chandu_._._ <chandan03chandu@gmail.com>
2023-12-19 11:30:29 +0530

    front end is done half the way

---
+++ src/components/Home/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer> This website is made as a Final year Project by <span> Anirudh Tantry, Chandan Kumar M, Mahesh Ks. </span> </footer>
+  )
+}
+
+export default Footer
